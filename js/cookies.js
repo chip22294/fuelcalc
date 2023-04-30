aaaeaf7d0b8c9c7a0dde247a5bf16a98936be8e2
chip22294 <chip22294@gmail.com>
2023-04-30 20:11:25 +0300

    Some CSS changes and bug fixes

--- js/cookies.js
+++ js/cookies.js
@@ -3,7 +3,8 @@ function setCookie(cname, cvalue, exdays) {
   const d = new Date();
   d.setTime(d.getTime() + (exdays*24*60*60*1000));
   let expires = "expires="+ d.toUTCString();
-  document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/; SameSite=None; Secure";
+  // document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/; SameSite=None; Secure";
+  document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/; SameSite=Strict";
 }
 
 function getCookie(cname) {
